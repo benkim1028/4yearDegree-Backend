3be8d76dc0efe081a9a71769607579e239de8cc2
Kwangsoo Yeo <ksyeo1996@gmail.com>
2017-03-19 02:40:22 -0700

    .

--- src/models/department.js
+++ src/models/department.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 const DepartmentSchema = new Schema({
     name: {required: true, type: String},
     href: {required: true, type: String},
-    faculty: {required: true, type: String},
-    majors: [{type: Schema.Types.ObjectId, ref: 'Major'}]
+    faculty: {required: true, type: String}
 });
 
 module.exports = mongoose.model('Department', DepartmentSchema);
