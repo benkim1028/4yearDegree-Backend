1d2d306fc35b6c65db057fe40a7e5144732f9cdd
Kwangsoo Yeo <ksyeo1996@gmail.com>
2017-03-18 18:29:14 -0700

    mongodb configs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Intervol Backend
 
 1. Run npm install
-2. Run npm start
+2. Run npm start or npm run start-dev to run with nodemon
 3. Open [localhost](http://localhost:3001/api)
 
---
+++ src/models/faculty.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const FacultySchema = new Schema({
+    name: {required: true, type: String},
+    href: {required: true, type: String},
+    departments: []
+});
+
+module.exports = mongoose.model('Faculty', FacultySchema);
